33ea01502e17a37b40a45cb89c6d93e061460233
czewski <eduardohb10@hotmail.com>
2022-08-04 09:04:11 -0300

    fix: added some logging options

--- src/recbole/runmodel.py
+++ src/recbole/runmodel.py
@@ -11,7 +11,7 @@ from recbole.data import create_dataset, data_preparation
 
 import csv   
 from datetime import datetime
-from sklearn.model_selection import KFold
+
 
 parameter_dict = {
     'seed': 1,
@@ -23,29 +23,36 @@ parameter_dict = {
     'TIME_FIELD': 'timestamp',
     'shuffle': False,  # Whether or not to shuffle the training data before each epoch
 
+    #data
+    'data_path': '/home/eduardo/projects/MovieLens-100k/dataset/ml100kfolds',
+    #'dataset': 'u1',
+    'benchmark_filename': ['base', 'real_valid', 'real_test'], #train, valid, test
+    'load_col':{'inter':['user_id', 'item_id', 'rating', 'timestamp']},
+
     # Train
     'epochs': 50,
     'learner': 'adam',  # ['adam', 'sgd', 'adagrad', 'rmsprop', 'sparse_adam']
     'learning_rate': 0.001,
     'train_batch_size': 2048,
 
-
     #Evaluation
     'metrics':['Recall', 'MRR', 'NDCG', 'Hit', 'Precision', 'MAP', 'TailPercentage'], # 'AUC', 'MAE', 'RMSE'
     'valid_metric': 'MRR@10',
-    'eval_args':{'split': {'RS': [0.8,0.1,0.1]}, 'group_by': 'user', 'order': 'RO', 'mode': 'full'}, #{'RS': [0.8,0.1,0.1]} {'LS': 'valid_and_test'}
     'topk': 10 
+    #'eval_args':{'split': {'RS': [0.8,0.1,0.1]}, 'group_by': 'user', 'order': 'RO', 'mode': 'full'}, #{'RS': [0.8,0.1,0.1]} {'LS': 'valid_and_test'}
     #load_split_dataloaders
 }
 
 if __name__ == '__main__':
+    #--dataset u1
+
     models = [general_recommender.Pop, general_recommender.ItemKNN, general_recommender.BPR, general_recommender.ConvNCF, general_recommender.MultiDAE, general_recommender.MultiVAE, general_recommender.CDAE]
 
     for model in models: 
-        
-        config = Config(model=model, dataset='ml-100k',
+        config = Config(model=model,
                         config_dict=parameter_dict)
         init_seed(config['seed'], config['reproducibility'])
+        #config['dataset'] = 'u1'
 
         # logger initialization
         init_logger(config)
@@ -81,7 +88,7 @@ if __name__ == '__main__':
         logger.info('model name: {}'.format(model.__class__.__name__))
 
 
-        fields=[model.__class__.__name__,best_valid_result,test_result, datetime.now()]
+        fields=[model.__class__.__name__,config['dataset'],datetime.now(), best_valid_result,test_result, ]  #log location, model location and name
         with open(r'data.csv', 'a') as f:
             writer = csv.writer(f)
             writer.writerow(fields)
